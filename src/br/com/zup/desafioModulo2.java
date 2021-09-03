9ca191d3e1cf37ff3e6ff33456012c206a4ed64a
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-03 13:04:33 -0300

    Inserindo 1 funcionário na lista 1 (Entrega minima)

--- src/br/com/zup/desafioModulo2.java
+++ src/br/com/zup/desafioModulo2.java
@@ -1,5 +1,31 @@
 package br.com.zup;
 
-public class desafioModulo2 {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class desafioModulo2 {
+    public static void main(String[] args) {
+        //Criando estruturas
+
+        Scanner leitor = new Scanner(System.in);
+        Map<String, String> listaDeFuncionaros = new HashMap<String, String>();
+
+        //Receber os dados
+
+        System.out.println("      Listamento dos Funcionários");
+        System.out.println("Digite o nome completo do funcioário: ");
+        String nome = leitor.nextLine();
+        System.out.println("Digite o telefone do funcionário: ");
+        String telefone = leitor.nextLine();
+        System.out.println("digite o email do funcionário: ");
+        String email = leitor.nextLine();
+        System.out.println("Digite o CPF do funcionário: ");
+        String cpf = leitor.nextLine();
+
+        //Declarando qual será a chave
+
+        listaDeFuncionaros.put(cpf, " Nome: " + nome +" Telefone: " + telefone +" Email: "+ email );
+        System.out.println(listaDeFuncionaros);
+    }
 }
