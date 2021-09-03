6c430b5e0248a27a15950006fc3c022a4aa2a076
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-03 14:46:00 -0300

    Opção do menu 1

--- src/br/com/zup/desafioModulo2.java
+++ src/br/com/zup/desafioModulo2.java
@@ -10,7 +10,9 @@ public class desafioModulo2 {
 
         Scanner leitor = new Scanner(System.in);
         Map<String, String> listaDeFuncionaros = new HashMap<String, String>();
+        int opcao = 1;
 
+       /*
         //Receber os dados
 
         System.out.println("      Listamento dos Funcionários");
@@ -27,5 +29,35 @@ public class desafioModulo2 {
 
         listaDeFuncionaros.put(cpf, " Nome: " + nome +" Telefone: " + telefone +" Email: "+ email );
         System.out.println(listaDeFuncionaros);
+
+        */
+
+            // Fazendo a solicitação de opção usando laço
+        while (opcao >= 1 && opcao <= 3) {
+            System.out.println("Digite a opçãp desejada: ");
+            System.out.println("1) Adicionar mais funcionários \n"
+                    + "2) Exibir Lista \n"
+                    + "3) Encerrar ");
+            System.out.println("Digite: ");
+            opcao= leitor.nextInt();
+            switch (opcao){
+                case 1:
+                    System.out.println("      Listamento dos Funcionários");
+                    System.out.println("Digite o nome completo do funcioário: ");
+                    String nome = leitor.nextLine();
+                    System.out.println("Digite o telefone do funcionário: ");
+                    String telefone = leitor.nextLine();
+                    System.out.println("digite o email do funcionário: ");
+                    String email = leitor.nextLine();
+                    System.out.println("Digite o CPF do funcionário: ");
+                    String cpf = leitor.nextLine();
+
+                    listaDeFuncionaros.put(cpf, " Nome: " + nome +" Telefone: " + telefone +" Email: "+ email );
+                    System.out.println(listaDeFuncionaros);
+                    break;
+
+            }
+        }
+
     }
 }
