090554350d462d87972a2bd2227e1a392e51aa35
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-09-03 16:26:51 -0300

    Remover Funcionario

--- src/br/com/zup/desafioModulo2.java
+++ src/br/com/zup/desafioModulo2.java
@@ -12,32 +12,14 @@ public class desafioModulo2 {
         Map<String, String> listaDeFuncionaros = new HashMap<String, String>();
         int opcao = 1;
 
-       /*
-        //Receber os dados
-
-        System.out.println("      Listamento dos Funcionários");
-        System.out.println("Digite o nome completo do funcioário: ");
-        String nome = leitor.nextLine();
-        System.out.println("Digite o telefone do funcionário: ");
-        String telefone = leitor.nextLine();
-        System.out.println("digite o email do funcionário: ");
-        String email = leitor.nextLine();
-        System.out.println("Digite o CPF do funcionário: ");
-        String cpf = leitor.nextLine();
-
-        //Declarando qual será a chave
-
-        listaDeFuncionaros.put(cpf, " Nome: " + nome +" Telefone: " + telefone +" Email: "+ email );
-        System.out.println(listaDeFuncionaros);
-
-        */
 
         // Fazendo a solicitação de opção usando laço
         while (opcao >= 1 && opcao <= 3) {
             System.out.println("Digite a opçãp desejada: ");
             System.out.println("1) Adicionar mais funcionários \n"
                     + "2) Exibir Lista \n"
-                    + "3) Encerrar ");
+                    + "3) Excluir Funcionário da Lista \n"
+                    + "4) Encerrar ");
             System.out.println("Digite: ");
             opcao = leitor.nextInt();
             leitor.nextLine();
@@ -53,21 +35,33 @@ public class desafioModulo2 {
                     String email = leitor.nextLine();
                     System.out.println("Digite o CPF do funcionário: ");
                     String cpf = leitor.nextLine();
-                    // Exibindo lista
+                    // Exibindo lista e declarando a chave
                     listaDeFuncionaros.put(cpf, " Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
                     System.out.println(listaDeFuncionaros);
                     break;
                 case 2:
                     //exibindo lista
                     for (String exibicao : listaDeFuncionaros.keySet()) {
-                        System.out.println( listaDeFuncionaros.get(exibicao) + " CPF: " + exibicao);
+                        System.out.println(listaDeFuncionaros.get(exibicao) + " CPF: " + exibicao);
                     }
                     break;
                 case 3:
+                    System.out.println("Digite o CPF que será deletado:");
+                    String funcionarioDelCpf = leitor.nextLine();
+                    for (String removerCpf : listaDeFuncionaros.keySet()) {
+                        if (removerCpf.equals(funcionarioDelCpf)){
+                            System.out.println("Funcionario excluído com sucesso");
+                            listaDeFuncionaros.remove(funcionarioDelCpf);
+                        }
+
+                    }
+
+
+                    break;
+                case 4:
                     System.out.println("Encerrando o cadastro");
                     System.exit(0);
                     break;
-
 
             }
         }
